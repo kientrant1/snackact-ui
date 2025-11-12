ab99f7b803fc410330a91b0a6619b73cf7e7abf8
Kien Tran <>
2025-11-12 10:01:42 +0700

    test: update storybook for quiz components

--- src/stories/Quiz.tsx
+++ src/stories/Quiz.tsx
@@ -1,36 +1,95 @@
-import { QuestionNavigator, QuizHeader } from '@/index'
-import '@/styles'
+import {
+  QuestionCard,
+  QuestionNavigator,
+  QuizHeader,
+  QuizNavigation,
+  QuizResults,
+} from '@/index'
 import { answers, questions } from './data/quiz'
 
-export const Quiz = () => {
-  const handleReset = () => {
+import '@/styles'
+
+type QuizComponentName =
+  | 'ALL'
+  | 'QuizHeader'
+  | 'QuestionNavigator'
+  | 'QuestionCard'
+  | 'QuizNavigation'
+  | 'QuizResults'
+export interface QuizProps {
+  quizComponentName: QuizComponentName
+}
+
+export const Quiz = ({ quizComponentName }: QuizProps) => {
+  const isShowAllComponents = quizComponentName === 'ALL'
+  const tags = ['ALL', 'Theory', 'Practical']
+  const filterTag = tags[0]
+  const currrentIndex = 0
+  const totalQuestions = 1
+  const submitted = false
+
+  const handleAction = () => {
     throw new Error('Function not implemented.')
   }
 
   return (
     <div className="min-h-screen p-4">
       <div className="max-w-4xl mx-auto snackact-ui-dark-theme">
-        <QuizHeader
-          timer={0}
-          currentIndex={0}
-          totalQuestions={5}
-          submitted={false}
-          score={1}
-          progress={0}
-          onReset={handleReset}
-          onToggleNavigator={() => {}}
-        />
-        <QuestionNavigator
-          questions={questions}
-          currentIndex={0}
-          submitted={false}
-          answers={answers}
-          tags={['ALL', 'Theory', 'Practical']}
-          filterTag={'ALL'}
-          onFilterChange={() => {}}
-          onJumpToQuestion={() => {}}
-          getQuestionStatus={() => 'unanswered'}
-        />
+        {(isShowAllComponents || quizComponentName === 'QuizHeader') && (
+          <QuizHeader
+            timer={0}
+            currentIndex={currrentIndex}
+            totalQuestions={totalQuestions}
+            submitted={submitted}
+            score={1}
+            progress={0}
+            onReset={handleAction}
+            onToggleNavigator={handleAction}
+          />
+        )}
+        {(isShowAllComponents || quizComponentName === 'QuestionNavigator') && (
+          <QuestionNavigator
+            questions={questions}
+            currentIndex={currrentIndex}
+            submitted={submitted}
+            answers={answers}
+            tags={tags}
+            filterTag={filterTag}
+            onFilterChange={handleAction}
+            onJumpToQuestion={handleAction}
+            getQuestionStatus={handleAction}
+          />
+        )}
+        {(isShowAllComponents || quizComponentName === 'QuestionCard') && (
+          <QuestionCard
+            question={questions[0]}
+            answers={answers}
+            submitted={submitted}
+            isCorrect={true}
+            onAnswer={handleAction}
+          />
+        )}
+
+        {(isShowAllComponents || quizComponentName === 'QuizNavigation') && (
+          <QuizNavigation
+            currentIndex={currrentIndex}
+            totalQuestions={totalQuestions}
+            submitted={submitted}
+            onPrevious={handleAction}
+            onNext={handleAction}
+            onSubmit={handleAction}
+          />
+        )}
+
+        {(isShowAllComponents || quizComponentName === 'QuizResults') && (
+          <QuizResults
+            timer={0}
+            questions={questions}
+            answers={answers}
+            score={1}
+            isCorrect={() => true}
+          />
+        )}
       </div>
     </div>
   )
